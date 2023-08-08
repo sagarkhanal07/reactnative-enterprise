19d033339b6d2ddfec875fa1f3232a8941a66cbb
sagarkhanal07 <sagar@optimumfuturist.com>
2023-08-08 07:33:48 +0545

    refactor: replaced experimental app router with expo bare template

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
+    // [Web-only]: Enables CSS support in Metro.
+    isCSSEnabled: true,
 });
 
 module.exports = config;
