3b3b6fa41509e95b3d52b55eeb640c3fbc7c417d
sagarkhanal07 <sagar@optimumfuturist.com>
2023-08-19 21:10:48 +0545

    feat: type safe env implemented with t3-env and zod

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { env } from './src/env';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>APP NAME: {env.EXPO_PUBLIC_APP_NAME}</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ src/env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
+
+export const env = createEnv({
+  /*
+   * Specify what prefix the client-side variables must have.
+   * This is enforced both on type-level and at runtime.
+   */
+  clientPrefix: 'EXPO_PUBLIC_',
+  isServer: true, // Bypass server-side checks
+  client: {
+    EXPO_PUBLIC_APP_NAME: z.string().min(1),
+  },
+  /**
+   * What object holds the environment variables at runtime.
+   * Often `process.env` or `import.meta.env`
+   */
+  runtimeEnv: process.env,
+});
