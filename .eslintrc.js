c1cb511b14f02d65a0107c9b267c40538cdd1b72
sagarkhanal07 <sagar@optimumfuturist.com>
2023-08-19 20:18:41 +0545

    feat: auto sort import implemented with some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
   extends: ['universe/native', 'prettier'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^expo', '^react'], ['^@?\\w'], ['@/(.*)'], ['^[./]']],
+      },
+    ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['export', 'function'], next: '*' },
+      { blankLine: 'always', prev: '*', next: ['export', 'function'] },
+    ],
+  },
 };
