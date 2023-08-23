5d27710d29ad8767c2748738d0233ce66c7be768
sagarkhanal07 <sagar@optimumfuturist.com>
2023-08-23 11:20:41 +0545

    chore: fix peer versions

--- src/.storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-ondevice-controls',
-    '@storybook/addon-ondevice-actions',
-  ],
-};
