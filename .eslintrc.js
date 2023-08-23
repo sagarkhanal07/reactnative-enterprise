06d761fe1eb2d10088319ada693175ed1d5f2fd6
sagarkhanal07 <sagar@optimumfuturist.com>
2023-08-23 07:28:28 +0545

    chore: updated usage in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
         groups: [['^expo', '^react'], ['^@?\\w'], ['@/(.*)'], ['^[./]']], // sort order for imports
       },
     ],
-    'import/order': ['off'],
+    'import/order': ['off'], // keepingdefault rule off and replaced with simple import sort
     'import/newline-after-import': ['error', { count: 1 }], // new line after all the imports
     'padding-line-between-statements': [
       // blank line between statements
